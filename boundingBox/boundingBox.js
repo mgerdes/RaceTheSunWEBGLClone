a481d68a9aec0d47ab305304b5e6508aa494a99d
Michael Gerdes <mgerdes11@gmail.com>
2016-02-18 19:22:29 -0500

    Can check to see if bounding boxes collide.

--- boundingBox/boundingBox.js
+++ boundingBox/boundingBox.js
@@ -8,7 +8,34 @@ app.BoundingBox = function(position, scale) {
 };
 
 app.BoundingBox.prototype.doesCollideWith = function(boundingBox) {
+    var thisMinX = this.position.x - (this.scale.x / 2);
+    var thisMaxX = boundingBox.position.x + (boundingBox.scale.x / 2);
+    var otherMinX = boundingBox.position.x - (boundingBox.scale.x / 2);
+    var otherMaxX = boundingBox.position.x + (boundingBox.scale.x / 2);
 
+    var xCollide = ((thisMinX >= otherMinX) && (thisMinX <= otherMaxX)) ||
+        ((otherMinX >= thisMinX) && (otherMinX <= thisMaxX));
+    if (!xCollide) return false;
+
+    var thisMinY = this.position.y - (this.scale.y / 2);
+    var thisMaxY = boundingBox.position.y + (boundingBox.scale.y / 2);
+    var otherMinY = boundingBox.position.y - (boundingBox.scale.y / 2);
+    var otherMaxY = boundingBox.position.y + (boundingBox.scale.y / 2);
+
+    var yCollide = ((thisMinY >= otherMinY) && (thisMinY <= otherMaxY)) ||
+        ((otherMinY >= thisMinY) && (otherMinY <= thisMaxY));
+    if (!yCollide) return false;
+
+    var thisMinZ = this.position.z - (this.scale.z / 2);
+    var thisMaxZ = boundingBox.position.z + (boundingBox.scale.z / 2);
+    var otherMinZ = boundingBox.position.z - (boundingBox.scale.z / 2);
+    var otherMaxZ = boundingBox.position.z + (boundingBox.scale.z / 2);
+
+    var zCollide = ((thisMinZ >= otherMinZ) && (thisMinZ <= otherMaxZ)) ||
+        ((otherMinZ >= thisMinZ) && (otherMinZ <= thisMaxZ));
+    if (!zCollide) return false;
+
+    return true; 
 };
 
 app.BoundingBox.prototype.draw = function() {
